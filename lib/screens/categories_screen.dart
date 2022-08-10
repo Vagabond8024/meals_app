73e41d02327ed473f9906a649d2b32153cc0c621
Bohdan Shaban <lan8024@gmail.com>
2022-08-10 22:39:37 +0300

    added meail item widget

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/category_item.dart';
-import 'dummy_data.dart';
+import 'package:meals_app/screens/view/category_item.dart';
+
+import '../dummy_data.dart';
+
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
