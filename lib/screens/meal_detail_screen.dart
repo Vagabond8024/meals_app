b22d1d459dfabd5b126c8fa56eb0d341dd69f559
Bohdan Shaban <lan8024@gmail.com>
2022-08-14 19:27:07 +0300

    Added meals screen

---
+++ lib/screens/meal_detail_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MealDetailScreen extends StatelessWidget {
+  const MealDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mealId = ModalRoute.of(context)?.settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(title: Text('l')),
+      body: Container(),
+    );
+  }
+}
